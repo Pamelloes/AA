0e024e6591d3634325d230499c22f798fdd0a119
Pamelloes <pamelloes@gmail.com>
2014-11-02 00:28:38 -0500

    Added string processing method to interpreter.

--- interpreter/interpreter.hs
+++ interpreter/interpreter.hs
@@ -2,20 +2,34 @@
 import qualified  Data.Map as M
 import Opcodes
 
-hasOpcode :: [Bool] -> [Char] -> Bool
-hasOpcode [] _ = False
-hasOpcode _ [] = False
+import Debug.Trace
+
+-- NOTE: The program is assumed to be an infinitely long list (see language
+-- specification). If a non-infinite list is provided, behavior is undefined.
+
+-- (program,opcode) -> (program',hasOpcode)
+-- If program starts with opcode, program' has the opcode removed.
+-- Otherwise, program'=program
+hasOpcode :: [Bool] -> [Char] -> ([Bool],Bool)
+hasOpcode [] _ = ([],False)
 hasOpcode s k 
-  | length z==length pattern = fst (foldl (\(a,_) (x,y) -> (a&&x==y,False))
-                               (True,True) z) == True
-  | otherwise = False
+  | length z==length pattern && match = (reduced, True)
+  | otherwise = (s,False)
   where pattern=opcodes M.! k
         z = zip pattern s
+        match = fst (foldl (\(a,_) (x,y) -> (a&&x==y,False)) (True,False) z) == True
+        reduced = drop (length pattern) s
+
+-- Program -> (Program',String); Program' is program with the string removed.
+lstring :: [Bool] -> ([Bool],[Bool])
+lstring [] = ([],[])
+lstring s
+  | snd es == True = (fst es,[])
+  | snd cs == True = let (a,b)=lstring prog in (a,str++b)
+  where es=hasOpcode s "ES"
+        cs=hasOpcode s "CS"
+        (str,prog) = splitAt 4 (fst cs)
 
 main = do
-  print (hasOpcode [True,False,True] "ES")
-  print (hasOpcode [True,False,True] "CS")
-  print (hasOpcode [True,False,True] "ERN")
-  print (hasOpcode [True,False,True] "LI")
-  print (hasOpcode [True,False,True] "BLE")
-  print (hasOpcode [True,False,True,True] "BLE")
+  print (lstring [True,True,False,True,False,True,False,False,False,False,True,
+         True,True,True,True,False])
