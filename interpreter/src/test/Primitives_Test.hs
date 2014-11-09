92cd01c0e39ca1e5a193bac2e9f19eaaa1f37120
Pamelloes <pamelloes@gmail.com>
2014-11-08 23:57:10 -0600

    Revised TestException further. Completed String tests.

--- interpreter/src/test/Primitives_Test.hs
+++ interpreter/src/test/Primitives_Test.hs
@@ -14,7 +14,7 @@ import Control.Exception
 -- String Tests
 strError = ErrorCall "lstring: Reached program end"
 
-testEmpty = TestLabel "Test Empty" $
+testEmptyP = TestLabel "Test Empty Program"  $
   TestCase $ assertException strError (lstring [])
 testIncomplete = TestLabel "Test Incomplete" $
   TestCase $ assertException strError (lstring p)
@@ -23,8 +23,27 @@ testUnfinished = TestLabel "Test Unfinished" $
   TestCase $ assertException strError (lstring p)
   where p=(opcodes M.! "CS") ++ (replicate 4 True)
 
+testEmpty = TestLabel "Test Empty String" $
+  TestCase $ assertEqual "" ([],[]) (lstring p)
+  where p=opcodes M.! "ES"
+testL4 = TestLabel "Test String Length 4" $
+  TestCase $ assertEqual "" ([],str) (lstring p)
+  where str=[False,True,True,False]
+        p=(opcodes M.! "CS") ++ str ++ (opcodes M.! "ES")
+testL8 = TestLabel "Test String Length 8" $
+  TestCase $ assertEqual "" ([],s1++s2) (lstring p)
+  where s1=replicate 4 False
+        s2=[False,True,False,True]
+        p=(opcodes M.! "CS")++s1++(opcodes M.! "CS")++s2++(opcodes M.! "ES")
+testExtra = TestLabel "Test Program Deletion" $
+  TestCase $ assertEqual "" (p2,str) $ lstring (p1++p2)
+  where str=replicate 4 True
+        p1=(opcodes M.! "CS")++str++(opcodes M.! "ES")
+        p2=replicate 23 False
+
 strTests = TestLabel "String" $
-  TestList[ testEmpty, testIncomplete, testUnfinished]
+  TestList[ testEmptyP, testIncomplete, testUnfinished, testEmpty, testL4, testL8
+          , testExtra]
 
 mainList = TestLabel "Primitives" $ TestList [ strTests ]
 main = runTestTT $ TestList [ P.mainList, mainList]
--- interpreter/src/test/TestException.hs
+++ interpreter/src/test/TestException.hs
@@ -8,8 +8,13 @@ import Test.HUnit
 
 -- To use properly, "a" should be in the form "evaluate $ <statement>"
 assertException :: (Exception e, Eq e, NFData a) => e -> a -> IO ()
-assertException ex action =
-  handleJust isWanted (const $ return ()) $ do
-    action `deepseq` return ()
-    assertFailure $ "Expected exception: " ++ show ex
+assertException ex action = 
+  handle fail $ do
+    handleJust isWanted (const $ return ()) $ do
+      action `deepseq` return ()
+      failure
   where isWanted = guard . (== ex)
+        failure = assertFailure $ "Expected exception: " ++ show ex
+        fail :: SomeException -> IO ()
+        fail e = assertFailure $ "Got exception: " ++ show e
+          ++ "\nExpected exception: " ++ show ex
