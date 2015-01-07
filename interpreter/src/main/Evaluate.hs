0667c0f3f576e5cd53989e8760fd5c0e6f5c8567
Pamelloes <pamelloes@gmail.com>
2015-01-07 15:12:45 -0600

    Starting some work with evaluating a program.

--- interpreter/src/main/Evaluate.hs
+++ interpreter/src/main/Evaluate.hs
@@ -25,10 +25,13 @@ THE SOFTWARE.
 module Evaluate where
 
 import BitSeries
+import Control.Arrow
 import qualified Data.Map as M
 import DataType
 import Opcodes
 import Statement
+
+type State = (ANmsp,Namespaces)
 
 -- Namespace definitions
 type Namespaces = M.Map ANmsp DataType
@@ -37,9 +40,47 @@ defaultNamespace :: BitSeries -> Namespaces
 defaultNamespace p = M.fromList [([],(p,BStatement))]
 
 nmspValue :: ANmsp -> DataType -> Namespaces -> DataType
-nmspValue a d n = if M.member i n then n M.! i else (repeat Terminate,BString [])
-  where i=gnmsp a d
+nmspValue a b =  M.findWithDefault (repeat Terminate,BString []) (gnmsp a b)
+
+nmspValue' :: State -> DataType -> (State,DataType)
+nmspValue' a b = (a,nmspValue (fst a) b (snd a))
 
 nmspValueSet :: ANmsp -> DataType -> DataType -> Namespaces -> Namespaces
-nmspValueSet a d = M.insert i
-  where i = gnmsp a d
+nmspValueSet a b = M.insert $ gnmsp a b
+
+nmspValueSet' :: State -> DataType -> DataType -> (State,DataType)
+nmspValueSet' a b c = (s a,c)
+  where s = second . const $ nmspValueSet (fst a) b c (snd a)
+
+-- Utilities
+{-
+estring   a = first cstring   . (evaluate a)
+einteger  a = first cinteger  . (evaluate a)
+erational a = first crational . (evaluate a)
+enmsp     a = first cnmsp     . (evaluate a)
+-}
+
+-- Evaluate definitions
+evaluate :: Free Stmt () -> State -> (State,DataType)
+evaluate (Free (LS a))   s = (s,a)
+evaluate (Free (AS a b)) s = nmspValueSet' s3 av bv
+  where (s2,av) = evaluate a s
+        (s3,bv) = evaluate b s2
+evaluate (Free (RS a))   s = nmspValue' s2 av
+  where (s2,av) = evaluate a s
+{-
+evaluate (Free (RS a)) = "\nRetrieve: "++showProgram a
+showProgram (Free (ET a b)) = "\nExecute:\nNmsp: "++showProgram a++snd c
+  where c = foldl (\(i,s) a->(i+1,s++"\n("++show (i+1)++") "++showProgram a))
+                  (0,"") b
+showProgram (Free (SQ a b)) = "\nSequence:\n(1) "++showProgram a++"\b(2) "
+  ++showProgram b
+showProgram (Free (IF a b c)) = "\nIf: "++showProgram a++"\nThen: "++showProgram b
+  ++"\nElse: "++showProgram c
+showProgram (Free (DW a b)) = "\nDo: "++showProgram a++"\nWhile: "
+  ++showProgram b
+showProgram (Free (MSA a b)) = "\nOperation "++a++": "++showProgram b
+showProgram (Free (MSB a b c)) = "\nOperation "++a++":\n(1) "++showProgram b
+  ++"\n(2) "++showProgram c
+showProgram (Free (IOS a)) = "\nI/O: "++showProgram a
+-}
