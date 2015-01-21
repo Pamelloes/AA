9080c306bef40fb88d761118a66819bd627415fc
Pamelloes <pamelloes@gmail.com>
2015-01-20 22:52:12 -0600

    Started experimenting with new types for DataType.

--- interpreter/src/main/DataTypePrime.hs
+++ interpreter/src/main/DataTypePrime.hs
@@ -23,9 +23,13 @@ THE SOFTWARE.
 -- This module consists of an alternate implementation of the various primitives
 -- in the hope of creating a more elegant parsing system.
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 module DataTypePrime where
 
 import BitSeries
+import Control.Applicative
 import Control.Arrow
 import Control.Monad
 import Control.Monad.Writer.Lazy
@@ -34,22 +38,64 @@ import qualified Data.Map as M
 import Data.Typeable
 import Opcodes
 import Text.Parsec.Combinator
-import Text.Parsec.Prim
+import Text.Parsec.Prim (ParsecT, Parsec)
 
--- Namespace Types
+class Primitive a where
+  convert :: BitSeries -> a
+
+data DataType a = DataType { bs::BitSeries, prim::a }
+instance Functor DataType where
+  fmap f (DataType a b) = DataType a (f b)
+instance Applicative DataType where
+  pure = DataType []
+  a <*> b = DataType (bs a <> bs b) (prim a $ prim b)
+instance Monad DataType where
+  return = pure
+  a >>= f = let b = f (prim a) in DataType (bs a <> bs b) (prim b)
+instance MonadWriter BitSeries DataType where
+  tell s = DataType s ()
+  listen s = DataType (bs s) (prim s,bs s)
+  pass s = DataType ((snd $ prim s) (bs s)) (fst $ prim s)
+instance (Monoid a) => Monoid (DataType a) where
+  mempty = DataType [] (mempty)
+  a `mappend` b = DataType (bs a <> bs b) (prim a <> prim b)
+
+ptell :: (MonadWriter a m) => a -> m a
+ptell a = tell a >> return a
+
+--convert :: (Primitive b) => DataType a -> DataType b
+--convert (DataType a _) = (DataType a $ convert a)
+
+type BString = BitSeries
+instance Primitive BString where
+  convert = undefined
+
+type BInteger = Integer
+instance Primitive BInteger where
+  convert = undefined
+
+type BRational = (Integer,Integer)
+instance Primitive BRational where
+  convert = undefined
+
 data RNmspS = Child BitSeries | Parent deriving (Show,D.Data,Typeable)
 type RNmsp = [RNmspS]
 type ANmsp = [BitSeries]
--- Global Types
-data Primitive = BString BitSeries | BInteger Integer 
-               | BRational Integer Integer | BNmspId (Either ANmsp RNmsp) 
-               | BStatement deriving (Show,D.Data,Typeable)
-type DataType = (BitSeries,Primitive) 
-type DataType' = Writer BitSeries Primitive
+type BNmspId = Either ANmsp RNmsp
+instance Primitive BNmspId where
+  convert = undefined
+
+type BStatement = ()
+instance Primitive BStatement where
+  convert = undefined
 
 -- Strings
-pstring :: Parsec BitSeries u DataType
-pstring = undefined
+type BSInt u = Parsec BitSeries u (DataType ())
+pstring :: Parsec BitSeries u (DataType BString)
+pstring = (mconcat <$> (many cs)) <* ((tell <$> mopc "ES")::BSInt u)
+  where cs :: Parsec BitSeries u (DataType BitSeries)
+        cs = ((tell <$> mopc "CS")::BSInt u)
+             >> (ptell <$> replicateM 4 anyToken)
 
 -- Integers
 bsToInt :: [Bit] -> Integer
@@ -58,11 +104,11 @@ bsToInt x = val + 16*(bsToInt remainder)
   where (base,remainder) = splitAt 4 x
         val = foldl (\x y->2*x+(if y==T then 1 else 0)) 0 base
 
-pinteger :: Parsec BitSeries u DataType
+pinteger :: Parsec BitSeries u (DataType BInteger)
 pinteger = undefined
 
 -- Rationals
-prational :: Parsec BitSeries u DataType
+prational :: Parsec BitSeries u (DataType BRational)
 prational = undefined 
 
 -- Namespaces
@@ -72,5 +118,5 @@ panmsp = undefined
 prnmsp :: Parsec BitSeries u (BitSeries,RNmsp)
 prnmsp = undefined
 
-pnmsp :: Parsec BitSeries u DataType
+pnmsp :: Parsec BitSeries u (DataType BNmspId)
 pnmsp = undefined
